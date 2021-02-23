845938e6022ac1cc26f199a2734def026d2d96d8
Nicholas J Hill <nicholasjhill@live.co.uk>
2021-02-23 20:45:05 +0000

    NH ✨ Added section headings

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,15 +2,26 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('<App/>', () => {
-    it('should display a heading', () => {
-        render(<App />);
-        expect(screen.getByRole('heading', { name: 'Nicholas Hill' })).toBeInTheDocument();
-    });
+    describe('header', () => {
+        it('should display a heading and sub-heading', () => {
+            render(<App />);
+            expect(screen.getByRole('heading', { name: 'Nicholas Hill' })).toBeInTheDocument();
+            expect(screen.getByRole('heading', { name: 'Software Developer' })).toBeInTheDocument();
+        });
 
-    describe('contact buttons', () => {
         it.each([['Email'], ['Phone'], ['GitHub'], ['CV']])('should display a %s button', (buttonTitle: string) => {
             render(<App />);
             expect(screen.getByRole('button', { name: buttonTitle })).toBeInTheDocument();
         });
+    });
+
+    describe('sections', () => {
+        it.each([['Experience'], ['Education'], ['Projects'], ['Skills'], ['Achievements'], ['Involvements']])(
+            'should display a %s heading',
+            (heading: string) => {
+                render(<App />);
+                expect(screen.getByText(heading)).toBeInTheDocument();
+            },
+        );
     });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import { ReactElement } from 'react';
-import { Typography, Button, makeStyles } from '@material-ui/core';
-import { Email, Phone, GitHub, Description } from '@material-ui/icons';
+import { Box, Button, Card, CardHeader, Container, Typography, makeStyles } from '@material-ui/core';
+import { Description, Email, GitHub, Phone } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
     main: {
         height: '100vh',
         display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
         alignItems: 'center',
+    },
+    header: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    sections: {
+        height: '100%',
+        width: '300px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'stretch',
+    },
+    title: {
+        textAlign: 'center',
     },
     buttonGroup: {
         display: 'flex',
@@ -23,22 +38,51 @@ export default function App(): ReactElement {
     const classes = useStyles();
 
     return (
-        <main className={classes.main}>
-            <Typography variant="h1">Nicholas Hill</Typography>
-            <section className={classes.buttonGroup}>
-                <Button variant="contained" color="primary" startIcon={<Email />} className={classes.button}>
-                    Email
-                </Button>
-                <Button variant="contained" color="primary" startIcon={<Phone />} className={classes.button}>
-                    Phone
-                </Button>
-                <Button variant="contained" color="primary" startIcon={<GitHub />} className={classes.button}>
-                    GitHub
-                </Button>
-                <Button variant="contained" color="primary" startIcon={<Description />} className={classes.button}>
-                    CV
-                </Button>
-            </section>
-        </main>
+        <Container className={classes.main}>
+            <Box className={classes.sections}>
+                <Card>
+                    <CardHeader title="Experience" />
+                </Card>
+                <Card>
+                    <CardHeader title="Education" />
+                </Card>
+                <Card>
+                    <CardHeader title="Projects" />
+                </Card>
+            </Box>
+            <Box className={classes.header}>
+                <Typography variant="h2" component="h1" className={classes.title}>
+                    Nicholas Hill
+                </Typography>
+                <Typography variant="h6" component="h2" className={classes.title}>
+                    Software Developer
+                </Typography>
+                <Box className={classes.buttonGroup}>
+                    <Button variant="contained" color="primary" startIcon={<Email />} className={classes.button}>
+                        Email
+                    </Button>
+                    <Button variant="contained" color="primary" startIcon={<Phone />} className={classes.button}>
+                        Phone
+                    </Button>
+                    <Button variant="contained" color="primary" startIcon={<GitHub />} className={classes.button}>
+                        GitHub
+                    </Button>
+                    <Button variant="contained" color="primary" startIcon={<Description />} className={classes.button}>
+                        CV
+                    </Button>
+                </Box>
+            </Box>
+            <Box className={classes.sections}>
+                <Card>
+                    <CardHeader title="Skills" />
+                </Card>
+                <Card>
+                    <CardHeader title="Achievements" />
+                </Card>
+                <Card>
+                    <CardHeader title="Involvements" />
+                </Card>
+            </Box>
+        </Container>
     );
 }
